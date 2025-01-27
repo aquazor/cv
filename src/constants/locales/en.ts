59c0638351f0a660933d18eda37549d1c2625aac
aquazor <kornevskiy99@gmail.com>
2025-01-27 15:16:53 +0200

    feat: implement 404 page

--- src/constants/locales/en.ts
+++ src/constants/locales/en.ts
@@ -88,4 +88,5 @@ export const en = {
       },
     ],
   },
+  error: { home: 'Home' },
 };
--- src/constants/locales/uk.ts
+++ src/constants/locales/uk.ts
@@ -89,4 +89,7 @@ export const uk = {
       },
     ],
   },
+  error: {
+    home: 'На головну',
+  },
 };
